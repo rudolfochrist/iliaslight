f49c15a847441ebd690ce8ac28c9dccc072b9cc1
Sebastian (Rudolfo) Christ <rudolfo.christ@gmail.com>
2010-08-19 14:46:02 +0200

    Changes to display the types in the right order for the editing view

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -26,6 +26,32 @@ class ExercisesController < ApplicationController
   
   def edit
     @exercise = Exercise.find(params[:id])
+      mcs = @exercise.multiple_choices.reverse
+      scs = @exercise.single_choices.reverse
+      marks = @exercise.marktexts.reverse
+      cloz = @exercise.clozes.reverse
+      drops = @exercise.dropdowns.reverse
+      order = @exercise.type_sequence_positions
+      @type_order = Array.new
+      order.each do |type|
+        case type.type_name
+        when "multiple_choices"
+          obj = mcs.pop
+          @type_order << obj unless obj.nil?
+        when "single_choices"
+          obj = scs.pop
+          @type_order << obj unless obj.nil?
+        when "marktexts"
+          obj = marks.pop
+          @type_order << obj unless obj.nil?
+        when "clozes"
+          obj = cloz.pop
+          @type_order << obj unless obj.nil?
+        when "dropdowns"
+          obj = drops.pop
+          @type_order << obj unless obj.nil?
+        end
+      end
   end
   
   def update
