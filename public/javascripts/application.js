f65e54b7b949fc4aa7266f4a60249296715cbad3
Sebastian (Rudolfo) Christ <rudolfo.christ@gmail.com>
2010-08-22 02:18:35 +0200

    removed javascript validation, because ActiveRecord validates the form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,14 +8,3 @@ function add_fields(link, association, content) {
   var regexp = new RegExp("new_" + association, "g");
   $(link).parent().before(content.replace(regexp, new_id));
 }
-
-function validateForm(submitBtn) {
-	var chapter = document.getElementById("exercise_chapter");
-	if (chapter.value) {
-		return true;
-	};
-	
-	alert("Chapter cannot be blank!");
-	chapter.style.backgroundColor = "lightcoral";
-	return false;
-}
